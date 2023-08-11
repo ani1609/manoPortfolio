eb9c70a26345df1686c18f89419c8756483530aa
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2023-08-12 05:01:35 +0530

    file structure setup

---
+++ src/app/components/navbar.js
@@ -0,0 +1,14 @@
+import styles from '../styles/navbar.module.css';
+
+
+function Navbar() 
+{
+    return (
+        <div className={styles.navbar_parent}>
+
+        </div>
+    )
+}
+  
+
+export default Navbar;
